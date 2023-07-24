a7193941c50f9dac8cbda021048b5590a8995896
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-24 01:59:57 -0500

    showTabReach and showTabStencil

--- src/components/my-card/my-card.tsx
+++ src/components/my-card/my-card.tsx
@@ -10,12 +10,14 @@ export class MyCard {
   @Prop({ mutable: true }) userName: string;
 
   @State() APIData: string;
-  @State() showCard: boolean = true;
+  // @State() showCard: boolean = true;
+  @State() showReactTab: boolean = false;
+  @State() showStencilTab: boolean = false;
 
   changeState() {
     this.userName = 'name has been updated';
     this.APIData = 'we have data from api';
-    this.showCard = false;
+    // this.showCard = false;
   }
 
   @Watch('userName')
@@ -24,6 +26,19 @@ export class MyCard {
   }
   componentWillUpdate() {
     console.log('componentWillUpdate...!');
+  }
+
+  onContentChange(content: string) {
+    if (content == 'reacttab') {
+      this.showReactTab = true;
+      this.showStencilTab = false;
+    } else if (content == 'stenciltab') {
+      this.showReactTab = false;
+      this.showStencilTab = true;
+    } else {
+      this.showReactTab = false;
+      this.showStencilTab = false;
+    }
   }
 
   render() {
@@ -39,9 +54,6 @@ export class MyCard {
       </div>
     );
 
-    let contentToDisplay = '';
-    if (this.showCard) contentToDisplay = reactContent;
-
     let stencilContent = (
       <div>
         <div class="card-custom" id="stencil-div">
@@ -51,14 +63,25 @@ export class MyCard {
         <button class="btn-stencil small-btn">Get Stencil Users</button>
       </div>
     );
+
+    let contentToDisplay = '';
+    if (this.showReactTab) {
+      contentToDisplay = reactContent;
+    } else if (this.showStencilTab) {
+      contentToDisplay = stencilContent;
+    }
+
     let mainContent = (
       <div class="my-card-wrapper">
         {this.userName ? <h1>Hi, I am {this.userName} </h1> : ''}
         <h5>{this.APIData}</h5>
-        <button class="btn-stencil">Stencil</button>
-        <button class="btn-react">React</button>
+        <button class="btn-stencil" onClick={this.onContentChange.bind(this, 'reacttab')}>
+          Stencil
+        </button>
+        <button class="btn-react" onClick={this.onContentChange.bind(this, 'stenciltab')}>
+          React
+        </button>
         {contentToDisplay}
-        {stencilContent}
       </div>
     );
     return mainContent;
